1c1b0d79ba298fc27249150ae0d55e47228ac0de
David Ortiz <darkoosi11@gmail.com>
2026-09-15 12:31:03 -0500

    Delete Corte1/Tarea2/Condicional2.py

--- Corte1/Tarea2/Condicional2.py
+++
@@ -1,9 +0,0 @@
-precio = int(input("digite un numero "))
-
-if precio <= 1000:
-    print("Barato")
-elif precio > 1000 and precio <= 2000:
-    print("mediano") 
-elif precio > 2000 and precio <= 3000:
-else:
-    print("caro")
